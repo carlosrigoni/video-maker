3ea001cbfed1a95065a31c734392277de02ff2fc
Carlos Daneil Rigoni <danrigoni1982@gmail.com>
2020-01-26 22:11:06 -0300

    feat: add Watson natural language understanding service

--- robots/text.js
+++ robots/text.js
@@ -2,10 +2,21 @@ const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+ 
+const nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey,
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+  })
+
 async function robot(content) {
     await fetchContentFromWikipedia(content);
     sanitizeContent(content); 
     breakContentIntoSentences(content);
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
@@ -54,6 +65,39 @@ async function robot(content) {
             })
         })
     }
+
+    function limitMaximumSentences(content) {
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+    }
+
+    async function fetchKeywordsOfAllSentences(content) {
+        for (const sentence of content.sentences) {
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence) {
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            }, (error, response) => {
+                if (error) {
+                    reject(error)
+                    return
+                }
+                
+                const keywords = response.keywords.map((keyword) => {
+                    return keyword.text
+                })
+                
+                resolve(keywords)
+            })
+        })
+    }
 }
+    
 
 module.exports = robot
